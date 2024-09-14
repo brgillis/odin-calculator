57ccdc9db8f9dd53a31ce57d07940cf139329f61
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2024-09-14 10:49:25 +0100

    Connect number buttons so they'll update the display and stored number

--- script.js
+++ script.js
@@ -34,8 +34,64 @@ function operate(a, opp, b) {
   }
 }
 
+// Connections to nodes
+const calcDisplay = document.querySelector(".calc-display input");
+const calcButtons = document.querySelector(".calc-buttons");
 
-// Export the functions we've defined
+// Variables to track current state of the calculator
+
+const FIRST_NUMBER = 0;
+const OPP = 1;
+const SECOND_NUMBER = 2;
+
+let inputMode = FIRST_NUMBER;
+let displayText = "";
+let firstNumberString;
+let oppString;
+let secondNumberString;
+
+// Functions called as-needed
+function updateDisplay() {
+  if (displayText==="")
+    calcDisplay.value = "0";
+  else
+    calcDisplay.value = displayText;
+}
+
+function addToDisplay(s) {
+  displayText += s;
+  updateDisplay();
+}
+
+// Functions to be connected to button presses
+function pressNumberButton (e) {
+  // Update input mode - either continue first or second number, or switch from opp to second number
+  if (inputMode===OPP)
+    inputMode = SECOND_NUMBER;
+
+  // Get the number typed and add it to the display and whichever number we're currently typing
+  let digitString = e.target.textContent;
+  addToDisplay(digitString);
+
+  if (inputMode===FIRST_NUMBER) {
+    firstNumberString += digitString;
+  } else {
+    secondNumberString += digitString;
+  }
+
+}
+
+// Connect functions to each button
+for (const buttonRow of calcButtons.children) {
+  for (const button of buttonRow.children) {
+    buttonClasses = Array.from(button.classList);
+    if (buttonClasses.includes("number")) {
+      button.addEventListener("click", pressNumberButton);
+    } // TODO else...
+  }
+}
+
+// Export the functions we've defined so they can be tested
 module.exports = {
   add,
   subtract,
